d95cde57e25b72e664e0ba022df12b1b00a9573b
siddacious <nospam187+github@gmail.com>
2019-08-27 19:45:20 -0700

    x,y,z working?\! needs scaling

--- adafruit_tlv493d.py
+++ adafruit_tlv493d.py
@@ -46,7 +46,7 @@ Adafruit's TLV493D Breakout https://adafruit.com/products
 * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
-
+import struct
 import adafruit_bus_device.i2c_device as i2cdevice
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_TLV493D.git"
@@ -79,8 +79,8 @@ class TLV493D:
         "INT":(1, 0x04, 2),
         "FAST":(1, 0x02, 1),
         "LOWPOWER":(1, 0x01, 0),
-        "TEMP_EN":(3, 0x80, 7),
-        "LOWPOWER_PERIOD":(3, 0x40, 6),
+        "TEMP_DISABLE":(3, 0x80, 7),
+        "LP_PERIOD":(3, 0x40, 6),
         "POWERDOWN":(3, 0x20, 5),
         "RES1":(1, 0x18, 3),
         "RES2":(2, 0xFF, 0),
@@ -98,14 +98,27 @@ class TLV493D:
         # // copy factory settings to write registers
         # setRegBits(tlv493d::W_RES1, getRegBits(tlv493d::R_RES1)); THREE TIMES?
         # // enable parity detection
-        # setRegBits(tlv493d::W_PARITY_EN, 1);
+        
+        print("\nWrite buffer:")
+        self.print_bytes(self.write_buffer)
+        self._set_write_key('PARITY', 1);
+        print("\nWrite buffer:")
+        self.print_bytes(self.write_buffer)
         # // config sensor to lowpower mode
         # // also contains parity calculation and writeout to sensor
-        # setAccessMode(TLV493D_DEFAULTMODE);
+        # setAccessMode(TLV493D_DEFAULTMODE);        
+        self._set_write_key('PARITY', 1);
+        self._set_write_key('LOWPOWER', 1);
+        self._set_write_key('LP_PERIOD', 1);
+        print("\nWrite buffer:")
+        self.print_bytes(self.write_buffer)
+        self._write_i2c()
+        print("wrote output buffer out")
 
     def _read_i2c(self):
         with self.i2c_device as i2c:
             i2c.readinto(self.read_buffer)
+        # self.print_bytes(self.read_buffer)
 
     @staticmethod
     def print_bytes(bites):
@@ -113,12 +126,9 @@ class TLV493D:
         for byte in bites:
             print("%s (%s)"%(bin(byte), hex(byte)))
 
-    # def _write_i2c(self, obj, value):
-    #     buf = bytearray(1+struct.calcsize(self.format))
-    #     buf[0] = self.address
-    #     struct.pack_into(self.format, buf, 1, value)
-    #     with obj.i2c_device as i2c:
-    #         i2c.write(buf)
+    def _write_i2c(self):
+        with self.i2c_device as i2c:
+            i2c.write(self.write_buffer)
 
     def _setup_write_buffer(self):
         self._read_i2c()
@@ -136,12 +146,9 @@ class TLV493D:
         self.print_bytes(self.write_buffer)
 
     def _get_read_key(self, key):
-        print("%s -> read: %s write: %s"%(key, self.read_masks[key], self.write_masks[key]))
         read_byte_num, read_mask, read_shift = self.read_masks[key]
         raw_read_value = self.read_buffer[read_byte_num]
-        print("value: %s"%bin(raw_read_value))
         write_value = (raw_read_value & read_mask)>>read_shift
-        print("write val: %s"%bin(write_value))
         return write_value
 
     def _set_write_key(self, key, value):
@@ -150,3 +157,36 @@ class TLV493D:
         current_write_byte &= ~write_mask
         current_write_byte |= value<<write_shift
         self.write_buffer[write_byte_num] = current_write_byte
+
+    @property
+    def x(self):
+        self._read_i2c()
+        x_top = self._get_read_key("BX1")
+        x_bot = self._get_read_key("BX2")
+        x_val = x_top<<4 | x_bot
+        xtop = (x_val & 0xFF00)>>8
+        xbot = (x_val & 0x00FF)
+        binval = struct.unpack_from(">h", bytearray([xtop, xbot]))[0] 
+        return binval
+
+    @property
+    def y(self):
+        self._read_i2c()
+        y_top = self._get_read_key("BY1")
+        y_bot = self._get_read_key("BY2")
+        y_val = y_top<<4 | y_bot
+        ytop = (y_val & 0xFF00)>>8
+        ybot = (y_val & 0x00FF)
+        binval = struct.unpack_from(">h", bytearray([ytop, ybot]))[0] 
+        return binval
+
+    @property
+    def z(self):
+        self._read_i2c()
+        z_top = self._get_read_key("BZ1")
+        z_bot = self._get_read_key("BZ2")
+        z_val = z_top<<4 | z_bot
+        ztop = (z_val & 0xFF00)>>8
+        zbot = (z_val & 0x00FF)
+        binval = struct.unpack_from(">h", bytearray([ztop, zbot]))[0] 
+        return binval
