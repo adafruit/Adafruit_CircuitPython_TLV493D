eb4922cbcf9e1f95aeb334db578b0c1db4640503
dherrada <dylan.herrada@gmail.com>
2020-03-12 16:32:20 -0400

    Ran black, updated to pylint 2.x

--- adafruit_tlv493d.py
+++ adafruit_tlv493d.py
@@ -46,8 +46,10 @@ Adafruit's TLV493D Breakout https://adafruit.com/products
 
 import struct
 import adafruit_bus_device.i2c_device as i2cdevice
+
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_TLV493D.git"
+
 
 class TLV493D:
     """Driver for the TLV493D 3-axis Magnetometer.
@@ -70,21 +72,21 @@ class TLV493D:
         "POWERDOWNFLAG": (5, 0x10, 4),
         "RES1": (7, 0x18, 3),
         "RES2": (8, 0xFF, 0),
-        "RES3": (9, 0x1F, 0)
+        "RES3": (9, 0x1F, 0),
     }
 
     write_masks = {
-        "PARITY":(1, 0x80, 7),
-        "ADDR":(1, 0x60, 5),
-        "INT":(1, 0x04, 2),
-        "FAST":(1, 0x02, 1),
-        "LOWPOWER":(1, 0x01, 0),
-        "TEMP_DISABLE":(3, 0x80, 7),
-        "LP_PERIOD":(3, 0x40, 6),
-        "POWERDOWN":(3, 0x20, 5),
-        "RES1":(1, 0x18, 3),
-        "RES2":(2, 0xFF, 0),
-        "RES3":(3, 0x1F, 0)
+        "PARITY": (1, 0x80, 7),
+        "ADDR": (1, 0x60, 5),
+        "INT": (1, 0x04, 2),
+        "FAST": (1, 0x02, 1),
+        "LOWPOWER": (1, 0x01, 0),
+        "TEMP_DISABLE": (3, 0x80, 7),
+        "LP_PERIOD": (3, 0x40, 6),
+        "POWERDOWN": (3, 0x20, 5),
+        "RES1": (1, 0x18, 3),
+        "RES2": (2, 0xFF, 0),
+        "RES3": (3, 0x1F, 0),
     }
 
     def __init__(self, i2c_bus):
@@ -96,10 +98,10 @@ class TLV493D:
         self._setup_write_buffer()
 
         # setup MASTERCONTROLLEDMODE which takes a measurement for every read
-        self._set_write_key('PARITY', 1)
-        self._set_write_key('PARITY', 1)
-        self._set_write_key('LOWPOWER', 1)
-        self._set_write_key('LP_PERIOD', 1)
+        self._set_write_key("PARITY", 1)
+        self._set_write_key("PARITY", 1)
+        self._set_write_key("LOWPOWER", 1)
+        self._set_write_key("LP_PERIOD", 1)
         self._write_i2c()
 
     def _read_i2c(self):
@@ -113,21 +115,21 @@ class TLV493D:
 
     def _setup_write_buffer(self):
         self._read_i2c()
-        for key in ['RES1', 'RES2', 'RES3']:
+        for key in ["RES1", "RES2", "RES3"]:
             write_value = self._get_read_key(key)
             self._set_write_key(key, write_value)
 
     def _get_read_key(self, key):
         read_byte_num, read_mask, read_shift = self.read_masks[key]
         raw_read_value = self.read_buffer[read_byte_num]
-        write_value = (raw_read_value & read_mask)>>read_shift
+        write_value = (raw_read_value & read_mask) >> read_shift
         return write_value
 
     def _set_write_key(self, key, value):
         write_byte_num, write_mask, write_shift = self.write_masks[key]
         current_write_byte = self.write_buffer[write_byte_num]
         current_write_byte &= ~write_mask
-        current_write_byte |= value<<write_shift
+        current_write_byte |= value << write_shift
         self.write_buffer[write_byte_num] = current_write_byte
 
     @property
@@ -135,19 +137,21 @@ class TLV493D:
         """The processed magnetometer sensor values.
         A 3-tuple of X, Y, Z axis values in microteslas that are signed floats.
         """
-        self._read_i2c() # update read registers
+        self._read_i2c()  # update read registers
         x_top = self._get_read_key("BX1")
-        x_bot = ((self._get_read_key("BX2") << 4) & 0xFF)
+        x_bot = (self._get_read_key("BX2") << 4) & 0xFF
         y_top = self._get_read_key("BY1")
-        y_bot = ((self._get_read_key("BY2") << 4) & 0xFF)
+        y_bot = (self._get_read_key("BY2") << 4) & 0xFF
         z_top = self._get_read_key("BZ1")
-        z_bot = ((self._get_read_key("BZ2") << 4) & 0xFF)
+        z_bot = (self._get_read_key("BZ2") << 4) & 0xFF
 
-        return (self._unpack_and_scale(x_top, x_bot),
-                self._unpack_and_scale(y_top, y_bot),
-                self._unpack_and_scale(z_top, z_bot))
+        return (
+            self._unpack_and_scale(x_top, x_bot),
+            self._unpack_and_scale(y_top, y_bot),
+            self._unpack_and_scale(z_top, z_bot),
+        )
 
-    def _unpack_and_scale(self, top, bottom): #pylint: disable=no-self-use
+    def _unpack_and_scale(self, top, bottom):  # pylint: disable=no-self-use
         binval = struct.unpack_from(">h", bytearray([top, bottom]))[0]
-        binval = binval >>4
+        binval = binval >> 4
         return binval * 0.098
--- examples/tlv493d_simpletest.py
+++ examples/tlv493d_simpletest.py
@@ -7,5 +7,5 @@ i2c = busio.I2C(board.SCL, board.SDA)
 tlv = adafruit_tlv493d.TLV493D(i2c)
 
 while True:
-    print("X: %s, Y: %s, Z: %s mT"%tlv.magnetic)
+    print("X: %s, Y: %s, Z: %s mT" % tlv.magnetic)
     time.sleep(1)
